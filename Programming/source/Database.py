0223ff2b01d41a5d30ec5fffb4f548a4de60f6e8
zhangxinhui02 <zhangxinhui02@qq.com>
2022-06-10 21:42:16 +0800

    完成软件基础题目

---
+++ Programming/source/Database.py
@@ -0,0 +1,155 @@
+import pymysql
+
+
+class Database:
+    """
+    封装了pymysql模块的类，用于辅助操作数据库
+    没有SQL注入的防范措施（考核不至于吧）
+    """
+    def __init__(self, host, user, password, database, port=3306):
+        """
+        初始化数据库对象
+        :param host: 数据库地址
+        :param user: 用户名
+        :param password: 密码
+        :param database: 数据库名
+        :param port: 端口
+        """
+        self.available = False
+        print('[Database] Connecting to MySQL...')
+        self.__host = host
+        self.__user = user
+        self.__password = password
+        self.__name = database
+        self.__port = port
+        try:
+            self.db = pymysql.connect(host=self.__host,
+                                      user=self.__user,
+                                      passwd=self.__password,
+                                      port=self.__port)
+            self.cur = self.db.cursor()
+            print('[Database] Initialize database...')
+            self.cur.execute('show databases;')
+            self.__database_list = list(self.cur.fetchall())
+            # 判断数据库是不是已经存在，不存在就新建数据库
+            if (self.__name.lower(),) in self.__database_list:
+                print('[Database] Database: ' + self.__name + ' already exists.')
+            else:
+                self.cur.execute('create database ' + self.__name)
+                self.db.commit()
+                print('[Database] Database: ' + self.__name + ' does not exist. Created successfully.')
+            self.cur.execute('use ' + self.__name + ';')
+            # 如果不存在数据表就新建表
+            self.__cmd = """create table if not exists students(
+            id int unsigned not null,
+            name varchar(10) not null,
+            sex int unsigned not null,
+            school varchar(30) not null,
+            major varchar(30) not null,
+            grade int unsigned not null
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+            """
+            self.cur.execute(self.__cmd)
+            self.db.commit()
+            self.available = True
+        except Exception as e:
+            print(e)
+
+    def add_data(self, stu_id, name, sex, school, major, grade):
+        """
+        新增数据
+        :param stu_id: 学号
+        :param name: 姓名
+        :param sex: 性别
+        :param school: 学院
+        :param major: 专业
+        :param grade: 年级
+        :return: None
+        """
+        __data_list = list(self.get_data('id', stu_id))
+        if len(__data_list) != 0:
+            print('[Database] Error: There is student with the same ID!')
+            return
+        self.__cmd = """insert into students
+        (id, name, sex, school, major, grade)
+        value
+        ( {0}, '{1}', {2}, '{3}', '{4}', {5} );
+        """.format(str(stu_id), name, str(sex), school, major, str(grade))
+        try:
+            self.cur.execute(self.__cmd)
+            self.db.commit()
+            print('[Database] Successfully added student with ID ' + str(stu_id))
+        except Exception as e:
+            print('[Database] Error:')
+            print(e)
+
+    def get_data(self, label, obj, order=False, field=None, way='ASC'):
+        """
+        查询指定的数据
+        :param label: 要查询的属性
+        :param obj: 查询值
+        :param order: 是否排序
+        :param field: 要排序的属性
+        :param way: 升序或降序（默认升序）[ASC/DESC]
+        :return:匹配到的列表
+        """
+        if order:
+            self.__cmd = """select * from students where {0}='{1}' order by {2} {3};
+                        """.format(label, str(obj), field, way)
+        else:
+            self.__cmd = """select * from students where {0}='{1}';
+            """.format(label, str(obj))
+        self.cur.execute(self.__cmd)
+        return list(self.cur.fetchall())
+
+    def get_all_data(self):
+        """
+        获取全部数据
+        :return: 全部数据的列表
+        """
+        self.__cmd = """select * from students;
+        """
+        self.cur.execute(self.__cmd)
+        return list(self.cur.fetchall())
+
+    def remove_data(self, stu_id):
+        """
+        删除数据
+        :param stu_id: 学号
+        :return: None
+        """
+        try:
+            __data_list = list(self.get_data('id', stu_id))
+            if len(__data_list) == 0:
+                print('[Database] Error: There is no student with such ID!')
+                return
+            self.__cmd = """delete from students where id={0};
+            """.format(str(stu_id))
+            self.cur.execute(self.__cmd)
+            self.db.commit()
+            print('[Database] Successfully removed student with ID ' + str(stu_id))
+        except Exception as e:
+            print('[Database] Error:')
+            print(e)
+
+    def update_data(self, stu_id, label, obj):
+        """
+        修改数据
+        :param stu_id: 学号
+        :param label: 属性
+        :param obj: 修改的内容
+        :return: None
+        """
+        try:
+            __data_list = list(self.get_data('id', stu_id))
+            if len(__data_list) == 0:
+                print('[Database] Error: There is no student with such ID!')
+                return
+            self.__cmd = """update students set {0}='{1}' where id={2};
+            """.format(label, obj, stu_id)
+            self.cur.execute(self.__cmd)
+            self.db.commit()
+            print('[Database] Successfully updated student with ID ' + str(stu_id))
+        except Exception as e:
+            print('[Database] Error:')
+            print(e)
---
+++ Programming/source/main.py
@@ -0,0 +1,135 @@
+#!/usr/bin/python3
+from Database import Database
+
+# 填写数据库基本信息
+database_host = "127.0.0.1"
+database_user = ""
+database_password = ""
+database_port = 3306
+database_name = "StudentsData"
+
+
+def show_all():
+    all_data_list = db.get_all_data()
+    print('\n[Manager] Information of all students:')
+    print('[Manager] (Sex 1 for Male, 2 for Female)')
+    print('\tID\tName\tSex\tSchool\tMajor\tGrade')
+    for i in all_data_list:
+        print('\t{0}\t{1}\t\t{2}\t{3}\t{4}\t{5}'.format(i[0], i[1], i[2], i[3], i[4], i[5]))
+    cmd = input('\nPress Enter...')
+
+
+def add():
+    print('\n[Manager] Add a new student:')
+    stu_id = input('\tInput student\'s ID: ')
+    try:
+        stu_id = int(stu_id)
+    except Exception as e:
+        print(e)
+        print('[Manager] Error: Not an int value! ')
+        return
+    name = input('\tInput student\'s name: ')
+    sex = input('\tInput student\'s sex: (1 for Male, 2 for Female)')
+    try:
+        sex = int(sex)
+        if sex != 1 and sex != 2:
+            print('[Manager] Error: Not a correct value for sex! ')
+            return
+    except Exception as e:
+        print(e)
+        print('[Manager] Error: Not an int value! ')
+        return
+    school = input('\tInput student\'s school: ')
+    major = input('\tInput student\'s major: ')
+    grade = input('\tInput student\'s grade: ')
+    try:
+        grade = int(grade)
+    except Exception as e:
+        print(e)
+        print('[Manager] Error: Not an int value! ')
+        return
+    db.add_data(stu_id, name, sex, school, major, grade)
+    cmd = input('\nPress Enter...')
+
+
+def update():
+    print('\n[Manager] Update a student\'s information:')
+    stu_id = int(input('\tInput student\'s ID: '))
+    label = input('\tInput which label to update [id, name, sex, school, major, grade]: ')
+    obj = input('\tInput the value after change: ')
+    db.update_data(stu_id, label, obj)
+    cmd = input('\nPress Enter...')
+
+
+def remove():
+    print('\n[Manager] Remove a student:')
+    stu_id = int(input('\tInput the ID of student who will be removed: '))
+    db.remove_data(stu_id)
+    cmd = input('\nPress Enter...')
+
+
+def search():
+    print('\n[Manager] Search for some students:')
+    field = ''
+    way = ''
+    label = input('\tInput which label to search [id, name, sex, school, major, grade]: ')
+    obj = input('\tInput the value for search: ')
+    order = input('\tOrder the list? [Y/N]: ')
+    if order in 'Yy':
+        order = True
+    else:
+        order = False
+    if order:
+        field = input('\tInput which label to order [id, name, sex, school, major, grade]: ')
+        way = input('\tChoose how to order (1 for ASC, 2 for DESC): ')
+        if way == '2':
+            way = 'DESC'
+        else:
+            way = 'ASC'
+    data_list = db.get_data(label, obj, order, field, way)
+    print('\tID\tName\tSex\tSchool\tMajor\tGrade')
+    for i in data_list:
+        print('\t{0}\t{1}\t\t{2}\t{3}\t{4}\t{5}'.format(i[0], i[1], i[2], i[3], i[4], i[5]))
+    cmd = input('\nPress Enter...')
+
+
+def menu():
+    print('\n[Manager] Menu of Manage System:')
+    print('\t1. Show information of all students.')
+    print('\t2. Add a new student.')
+    print('\t3. Update a student\'s information.')
+    print('\t4. Remove a student.')
+    print('\t5. Search for some students.')
+    print('\t6. Exit.')
+    cmd = input('Please choose: ')
+    if cmd == '1':
+        show_all()
+    elif cmd == '2':
+        add()
+    elif cmd == '3':
+        update()
+    elif cmd == '4':
+        remove()
+    elif cmd == '5':
+        search()
+    elif cmd == '6':
+        print('[Manager] Bye')
+        exit(0)
+
+
+if __name__ == '__main__':
+    print('Students Data Manage System')
+    print('by zhangxinhui02')
+    print('GitHub: https://github.com/zhangxinhui02/RoboticsAssociationAssessment2022\n')
+
+    print('[Manager] Connecting to database...')
+    db = Database(database_host,
+                  database_user,
+                  database_password,
+                  database_name,
+                  database_port)
+    if db.available is not True:
+        print('[Manager] Database is not available!')
+        exit(0)
+    while True:
+        menu()
